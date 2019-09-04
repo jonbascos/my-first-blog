e58cc14bfbe63f5921ec3c681a7a8edab1855025
jonbascos <jon@jonbascos.com>
2019-09-04 15:39:50 -0700

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w_h38h=)=i8v5(%0=c=k&e)g(%r)r+5bn!%z%&ybd#=i=76_#h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
